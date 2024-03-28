27bb9197636535fd942b759f6b24bc455d732de0
Sami Laine <sami.v.laine@aalto.fi>
2024-03-28 17:08:15 +0200

    Bump to 0.9.2.dev501

--- src/course_list/index.ts
+++ src/course_list/index.ts
@@ -70,7 +70,7 @@ export class CourseListWidget extends Widget {
   }
 
     checkNbGraderVersion() {
-      let nbgrader_version = '0.9.2.dev500';
+      let nbgrader_version = '0.9.2.dev501';
       requestAPI<any>('nbgrader_version?version='+nbgrader_version)
           .then(response => {
               if (!response['success']) {
